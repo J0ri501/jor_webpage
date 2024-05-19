2c16b5fcd6614b605c87282981dbac097fe72b62
joris <lxlr722@gmail.com>
2024-05-19 15:16:52 -0500

    adding a bit of responsiveness(not finished)

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
-    typeWriter("about-me-content", "Hello, I'm Jorge, a mechatronics engineer with a passion for programming and robotics. I have experience in Python, C++, and Java, and I love working on innovative projects that combine my skills in mechanics, electronics, and software development.");
+    typeWriter("about-me-content", "1 Hello, I'm Jorge, a mechatronics engineer with a passion for programming and robotics. I have experience in Python, C++, and Java, and I love working on innovative projects that combine my skills in mechanics, electronics, and software development.");
 
-    const skills = ["Python", "C++", "Java", "Robotics", "Automation"];
+    const skills = ["1 Python", "2 C++", "3 Java", "4 Robotics", "5 Automation"];
     displaySkills("skills-tools-content", skills);
 });
 
